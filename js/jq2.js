cd610fb070b30372fbbb4f933b9a4fdc65c20e24
Maggie Wu <maggiejieyao@gmail.com>
2019-06-07 22:46:54 -0700

    added loading icon and added user profile intro

--- js/jq2.js
+++ js/jq2.js
@@ -28,6 +28,13 @@ function changeLimit(limit, username){
     user = user + limit
     $.ajax({
         url:user,
+        beforeSend:function(){
+            $('.loading').show();
+        },
+        complete:function(){
+            $('.loading').hide();
+        },
+        
         success: function(data){
             var insData = JSON.parse(data)
             console.log(insData.photoUrlHD)
@@ -38,11 +45,28 @@ function changeLimit(limit, username){
                 'height':150,
                 'border-radius': '50%'
             }).appendTo(profileCon);
-
+            
+            //add username
+            var userFullName = '<p id="u">' + insData.fullName + '</p>';
+            $(userFullName).appendTo($('.profileCon')).css({
+                'font-size':'2em',
+                'color':'gray',
+                'marginTop':'5px'
+            });
+            
+            var postList = insData.posts;
+            //add following and posts
+            var fp = '<p>' + postList.length + ' posts   |   ' + insData.followedBy + ' follows   |   ' + insData.following + ' following</p>';
+            $(fp).appendTo($('.profileCon')).css({
+                'font-size':'1.1em',
+                'color':'darkgray',
+                'marginTop':'-20px'
+            });
+            
             //add images to context
             var photoCon = $('.context');
             var carClass = $('<div class="card">');
-            var postList = insData.posts;
+            
             if(postList.length > 0){
                 for(var i = 0; i < postList.length; i++){
                 $('<img>').attr('src', postList[i].photoUrl).addClass("card-img-top").css({
@@ -77,45 +101,4 @@ function changeLimit(limit, username){
             }
         
     });
-       /*.done(function(data){
-        var insData = JSON.parse(data)
-        console.log(insData.photoUrlHD)
-        //add profile image
-        var profileCon = $('.profileCon');
-        $('<img>').attr('src', insData.photoUrlHD).css({
-            'width':150,
-            'height':150,
-            'border-radius': '50%'
-        }).appendTo(profileCon);
-        
-        //add images to context
-        var photoCon = $('.context');
-        var carClass = $('<div class="card">');
-        var postList = insData.posts;
-        if(postList.length > 0){
-            for(var i = 0; i < postList.length; i++){
-            $('<img>').attr('src', postList[i].photoUrl).addClass("card-img-top").css({
-                'width': 300,
-                'height': 300,
-                'border-radius':'5px'
-            }).appendTo(carClass).appendTo(photoCon);
-            }
-            ($(".proContainer")).append($('<input type="button" class="btn btn-light" id = "clearBtn" value="Clear All"/>')).css({
-                'text-align':'center',
-                'font-family': "'Nanum Brush Script', cursive",
-            });
-        }else{
-            photoCon.html("This account is private").css({
-                'font-family': "'Nanum Brush Script', cursive",
-                'font-size':'1.5em'
-            });
-        }
-        
-        //clear all button
-        $("#clearBtn").click(function(){
-            $(".context").html('');
-            $("#clearBtn").remove();
-             
-        });
-    });*/
 }
